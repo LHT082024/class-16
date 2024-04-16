82ff733e83225c1e816218f80d34e9e66d48f0a3
SapphireShy <lucielity@gmail.com>
2024-04-16 12:03:03 +0200

    descriptons

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 //sjekker om knappen er trykket og caller verdien i tekst elemnetet.
+
+//
 function addTodo() {
    event.preventDefault();
 
@@ -8,18 +10,26 @@ function addTodo() {
   const taskInput = document.querySelector("#taskInput").value;
   console.log(taskInput);
 
+  //createElement larger et nytt html element.
   const newListItems = document.createElement("li");
   console.log(newListItems);
 
+  //
   newListItems.textContent = taskInput;
 
 //henter ul elmentet
   const taskList = document.querySelector("#taskList");
   console.log(taskList);
 
-  newListItems.appendChild(taskList);
+  taskList.appendChild(newListItems);
 }
 
+//const er en erklæring av en variabel. 
+//taskButton er et variabel navn.
+// = å ærklere en verdi som skal lagre
+//doucment referer til HTML dokumentet
+//querySelector() henter noe fra HTML
+//; betyr at kodelinjen er stoppet. 
 const taskButton = document.querySelector("#taskButton");
 console.log(taskButton);
 
