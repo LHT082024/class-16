f5ae49a3ae0009f3661f3f2124e5b1b89339d07b
SapphireShy <lucielity@gmail.com>
2024-04-16 11:36:49 +0200

    fikser javascript

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello!');
+function addTodo() {
+   event.preventDefault();
+
+  console.log("is button clicked?");
+
+  const taskInput = document.querySelector("#taskInput").value;
+  console.log(taskInput);
+
+  const newListItems = document.createElement("li");
+  console.log(newListItems);
+
+  newListItems.textContent = taskInput;
+}
+
+const taskButton = document.querySelector("#taskButton");
+console.log(taskButton);
+
+taskButton.addEventListener("click", addTodo);
